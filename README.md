a4e6830f6a21a7db919d10af33a65e51be3204ef
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-02 18:09:25 -0500

    Initial setup, plus Hero component with animation

---
+++ README.md
@@ -0,0 +1,28 @@
+# Restaurant Demo
+
+<div align='center' width='100%'>
+  <img src='' alt='' style='box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05); border-radius: 0.5rem'>
+</div>
+
+<!-- <details>
+  <summary>Table of Contents</summary>
+
+  - [Live Demo](#live-demo)
+  - [Built With](#built-with)
+</details> -->
+
+## Live Demo
+
+<!-- [Restaurant Demo]() -->
+
+<!-- [![Netlify Status]()() -->
+
+## Built with
+- [React.js](https://reactjs.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [Framer Motion](https://www.framer.com/motion/)
+
+## Contact
+
+[Stan Clarke, Jr.](https://github.com/stanclarke-jr) - hi@stanclarkejr.ca
---
+++ src/components/Hero.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { heroData } from '../data';
+import Header from './Header';
+import { motion } from 'framer-motion';
+import { fadeIn, staggerContainer } from '../variants';
+
+const Hero = () => {
+  const { pretitle, title, subtitle, btnText } = heroData;
+
+  return (
+    <section className="min-h-[980px] bg-hero bg-cover bg-right">
+      <Header />
+      <div className="container mx-auto flex min-h-[980px] items-center justify-center">
+        <motion.div
+          variants={staggerContainer(0.3, 1)}
+          initial="hidden"
+          whileInView={'show'}
+          className="flex flex-col items-center text-center"
+        >
+          <motion.div
+            variants={fadeIn('down', 'tween', 0.2, 1.1)}
+            className="mb-1 font-primary text-[24px] italic text-white lg:text-[28px] lg:font-medium"
+          >
+            {pretitle}
+          </motion.div>
+          <motion.h1
+            variants={fadeIn('down', 'tween', 0.3, 1.1)}
+            className="h1 mb-5"
+          >
+            {title}
+          </motion.h1>
+          <motion.p
+            variants={fadeIn('down', 'tween', 0.4, 1.1)}
+            className="mb-8 max-w-[540px] text-white"
+          >
+            {subtitle}
+          </motion.p>
+          <motion.div variants={fadeIn('down', 'tween', 0.5, 1.1)}>
+            <button className="btn">{btnText}</button>
+          </motion.div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
