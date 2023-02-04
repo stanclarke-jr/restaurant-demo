65ca31c2ecb213350befc58f3c12b93b5017341c
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-04 16:08:22 -0500

    feat: Social media icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import { socialData } from '../data';
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex gap-x-[10px]">
+      {socialData.map((item, index) => {
+        return (
+          <a
+            key={index}
+            className="flex h-[35px] w-[35px] items-center justify-center rounded-full border border-white/20 text-sm transition-all hover:border-accent hover:text-accent"
+            href={item.href}
+          >
+            {item.icon}
+          </a>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Socials;
