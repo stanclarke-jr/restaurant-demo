c8c230062ed899b64208132eda87142596461bfb
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-04 15:57:26 -0500

    feat: Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import { navData } from '../data';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="= h-full w-full">
+      <ul className="flex h-full flex-col items-center justify-center gap-y-6">
+        {navData.map((item) => {
+          return (
+            <li key={item.name}>
+              <a
+                className="font-primary text-xl capitalize italic transition-all duration-300 hover:text-dark"
+                href={`${item.href}home`}
+              >
+                {item.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
