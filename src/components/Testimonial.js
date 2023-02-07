c701857ba97fc588fe35f9b9f239ab32878f6570
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-07 10:41:04 -0500

    feat: Testominal secition with carousel. TODO: animation

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import TestimonialCarousel from './TestimonialCarousel';
+import { testimonialData } from '../data';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  const { title, subtitle, modelImg, slider } = testimonialData;
+
+  return (
+    <section className="relative top-[340px] z-10 h-[800px] bg-testimonial bg-cover bg-no-repeat pt-[90px] md:pt-[120px] lg:top-[390px]">
+      <div className="container mx-auto">
+        {/* Content */}
+        <div className="flex flex-col items-center text-center capitalize">
+          <h2 className="h2 text-white">{title}</h2>
+          <p className="mb-8 capitalize text-white/70">{subtitle}</p>
+          <div className="mb-8">
+            <img src={modelImg} alt="" />
+          </div>
+        </div>
+        {/* Carousel */}
+        <div className="flex items-center justify-center">
+          <TestimonialCarousel slider={slider} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonial;
--- src/components/TestimonialCarousel.js
+++ src/components/TestimonialCarousel.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import '../slider.css';
 
-const TestimonialCarousel = () => {
-  return <div>TestimonialCarousel</div>;
+const TestimonialCarousel = ({ slider }) => {
+  return (
+    <Carousel
+      showThumbs={false}
+      showStatus={false}
+      showIndicators={false}
+      autoPlay={false}
+      infiniteLoop={true}
+      className="flex w-full max-w-[1024px] items-center justify-center "
+    >
+      {slider.map((item, index) => {
+        const { message, image, name, occupation } = item;
+
+        return (
+          <div key={index} className="mx-auto max-w-[840px] px-24 text-white">
+            <div className="mb-9 text-lg leading-snug lg:text-2xl">
+              {message}
+            </div>
+            <div className="mx-auto mb-3 max-w-[70px] lg:max-w-[100px]">
+              <img src={image} alt={name} />
+            </div>
+            <div className="text-[22px] font-bold text-accent">{name}</div>
+            <div className="font-medium italic">{occupation}</div>
+          </div>
+        );
+      })}
+    </Carousel>
+  );
 };
 
 export default TestimonialCarousel;
