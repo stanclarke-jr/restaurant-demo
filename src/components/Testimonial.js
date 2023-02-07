d504c931596a5e39843062d8281d9407b749586e
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-07 11:24:41 -0500

    feat: Testimonial section/componet animation done

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -10,17 +10,27 @@ const Testimonial = () => {
     <section className="relative top-[340px] z-10 h-[800px] bg-testimonial bg-cover bg-no-repeat pt-[90px] md:pt-[120px] lg:top-[390px]">
       <div className="container mx-auto">
         {/* Content */}
-        <div className="flex flex-col items-center text-center capitalize">
+        <motion.div
+          variants={fadeIn('up', 'tween', 0.2, 1.6)}
+          initial="hidden"
+          whileInView={'show'}
+          className="flex flex-col items-center text-center capitalize"
+        >
           <h2 className="h2 text-white">{title}</h2>
           <p className="mb-8 capitalize text-white/70">{subtitle}</p>
           <div className="mb-8">
             <img src={modelImg} alt="" />
           </div>
-        </div>
+        </motion.div>
         {/* Carousel */}
-        <div className="flex items-center justify-center">
+        <motion.div
+          variants={fadeIn('up', 'tween', 0.4, 1.6)}
+          initial="hidden"
+          whileInView={'show'}
+          className="flex items-center justify-center"
+        >
           <TestimonialCarousel slider={slider} />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
