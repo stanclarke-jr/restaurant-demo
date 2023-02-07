963943cf8031552a17570c04ae5dda1ae3404a0f
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-07 18:20:03 -0500

    feat: Footer section/component started - container styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import Newsletter from './Newsletter';
+import Copyright from './Copyright';
+import Socials from './Socials';
+
+import { footerData } from '../data';
+
+import { motion } from 'framer-motion';
+import { fadeIn, staggerContainerS } from '../variants';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const { contact, hours, socia } = footerData;
+  return (
+    <footer className="relative top-96 z-20 bg-dark pt-20 lg:min-h-[620px] lg:bg-transparent lg:bg-footer lg:bg-center lg:bg-no-repeat">
+      <div className="container mx-auto h-full">
+        {/* Newsletter & restaurant info*/}
+        <div>
+          <div>
+            <Newsletter />
+          </div>
+          {/* Restaurant info */}
+          <div>info</div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
