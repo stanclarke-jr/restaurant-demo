e21469c80403f8fe65089a2cb3f787a2de80f26c
Stan Clarke, Jr <stan@stanclarkejr.ca>
2023-02-08 13:21:22 -0500

    feat: Copyright section/component

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Copyright = () => {
-  return <div>Copyright</div>;
+  return (
+    <div className="border-t border-white/20">
+      <div className="container mx-auto">
+        <div className="py-6">
+          <p className="text-center text-base">
+            Copyright &copy; {new Date().getFullYear()} Bistro Cafe. All rights
+            reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Copyright;
